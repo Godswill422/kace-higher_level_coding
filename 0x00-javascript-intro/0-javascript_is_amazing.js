806d510ec764696a9741339cf606999056c22f22
Godswill <godswilsownlife@gmail.com>
2025-04-15 17:13:57 +0100

    task: add example

---
+++ 0x00-javascript-intro/0-javascript_is_amazing.js
@@ -0,0 +1,12 @@
+#! /usr/bin/node
+
+let name = 'Stephen';
+let firstName = 'Tekena';
+const lastName = 'Olali'
+console.log('My name is ' + name);
+
+name = 'Innocent';
+console.log('My name is ' + name);
+
+console.log('What\'s my name?');
+console.log(firstName + " " + lastName);
